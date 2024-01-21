98c1df5a09f93b665062c5c28b53603f089be248
SAHIL-KUMAR-EE <121251129+SAHIL-KUMAR-EE@users.noreply.github.com>
2024-01-22 00:29:46 +0530

    Updated popup.js
    Added Dark Mode Toggle

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,8 +1,32 @@
+// popup.js
 document.addEventListener('DOMContentLoaded', function () {
+    var clickCount = 0;
     document.getElementById("b1").onclick = function () {
-        console.log("Clicked ");
-        chrome.tabs.executeScript({
-            file: 'highlighter.js'
-        });
+        // clickCount toggles the button
+        if (clickCount % 2 === 0) {
+            document.getElementById("b_image").src = "button_on.svg";
+            chrome.tabs.executeScript({
+                file: 'highlighter.js'
+            });
+        } else {
+            document.getElementById("b_image").src = "button_off.svg";
+            chrome.tabs.reload();
+        }
+        clickCount++;
+    }
+    chrome.runtime.onMessage.addListener(function (message) {
+        if (message.action === 'sendMatchCounter') {
+            document.getElementById('count').textContent = message.matchCounter;
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("dark-mode").onclick = function () {
+        document.body.classList.toggle("light");
+        document.getElementById("extension_name").classList.toggle("light");
+        document.getElementById("b1").classList.toggle("light");
+        document.querySelector(".github-link").classList.toggle("light");
+        document.getElementById("dark-mode").classList.toggle("light");
     }
 });
