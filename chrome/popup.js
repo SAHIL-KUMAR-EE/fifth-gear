8bd9443d4552871c06aa948b1181b82aa8de7dcb
SAHIL-KUMAR-EE <121251129+SAHIL-KUMAR-EE@users.noreply.github.com>
2024-01-29 13:58:48 +0530

    Updated popup.js
    Collects data from higlighter.js and updates collapsible container list accordingly

--- chrome/popup.js
+++ chrome/popup.js
@@ -15,8 +15,23 @@ document.addEventListener('DOMContentLoaded', function () {
         clickCount++;
     }
     chrome.runtime.onMessage.addListener(function (message) {
-        if (message.action === 'sendMatchCounter') {
-            document.getElementById('count').textContent = message.matchCounter;
+        if (message.action === 'sendMatchData') {
+            document.getElementById('count').textContent = message.matchData.Total;
+            
+            const Categories = message.matchData.Categories;
+            const catList = document.getElementById("categoryList");
+
+            for (const category in Categories) {
+                const value = Categories[category];
+            
+                // Checks if dark pattern count is non-zero
+                if (value !== 0) {
+                  const listItem = document.createElement("li");
+                  listItem.textContent = `${category}: ${value}`;
+                  catList.appendChild(listItem);
+                }
+              }
+
         }
     });
 });
@@ -28,5 +43,31 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("b1").classList.toggle("light");
         document.querySelector(".github-link").classList.toggle("light");
         document.getElementById("dark-mode").classList.toggle("light");
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("categoryShow").onclick = function () {
+        var content = document.getElementById("collapsibleCategory");
+        var main_container = document.querySelector(".counter-container");
+        if (!content) {
+            return;
+        }
+
+        var body = document.body;
+
+        if (content.style.display === "flex") {
+            content.style.display = "none";
+            body.style.height = "400px";
+            main_container.style.flex = "0 1 auto";
+        } else {
+            main_container.style.display = "flex";
+            content.style.display = "flex";
+            main_container.style.flex = "1";
+            body.style.minHeight = "400px";
+            body.style.height = "auto";
+            main_container.style.marginBottom = "10px";
+            main_container.style.marginTop = "5px";
+        }
     }
 });
