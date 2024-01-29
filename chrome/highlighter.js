c737f463c4f4f7b66d9e86ee28126a631081a73e
SAHIL-KUMAR-EE <121251129+SAHIL-KUMAR-EE@users.noreply.github.com>
2024-01-29 13:57:08 +0530

    Updated highlighter.js
    Now sends categories along with total dark pattern count

--- chrome/highlighter.js
+++ chrome/highlighter.js
@@ -1,6 +1,7 @@
 // highlighter.js
 var fileUrl = chrome.runtime.getURL('dataset.txt');
 console.log(fileUrl);
+
 fetch(fileUrl)
     .then(response => response.text())
     .then(content => {
@@ -9,19 +10,46 @@ fetch(fileUrl)
       var words = arr.map(element => element.replace('\r', ''));
       var bodyText = document.body.innerHTML;
       var matchCounter = 0;
+      var categoryCounts = {
+        "Forced Action":0,
+        "False Reassurance":0,
+        "Misdirection":0,
+        "False Scarcity":0,
+        "Social Proof":0,
+        "Hidden Subscription":0,
+        "False Affiliation":0,
+        "Negative Option":0,
+        "Upselling":0,
+        "Positive Reinforcement":0,
+        "Urgency":0
+      }; // Object to store category-wise match counts
 
       for (var i = 0; i < words.length; i++) {
-        var regex = new RegExp(words[i], 'gi');
+        var wordData = words[i].split('@');
+        var word = wordData[0];
+        var category = wordData[1];
+
+        var regex = new RegExp(word, 'gi');
         bodyText = bodyText.replace(regex, function(match) {
           matchCounter++;
+
+          // Update category count
+          if (category) {
+            if (!categoryCounts[category]) {
+              categoryCounts[category] = 1;
+            } else {
+              categoryCounts[category]++;
+            }
+          }
+
           return '<mark>' + match + '</mark>';
         });
       }
-      // Sending dark patterns count to popup.js
-      sendMatchCounter(matchCounter);
+      // Sending match data and categories to popup.js
+      sendMatchData({ Total: matchCounter, Categories:categoryCounts });
       document.body.innerHTML = bodyText;
     });
 
-function sendMatchCounter(counter) {
-  chrome.runtime.sendMessage({ action: 'sendMatchCounter', matchCounter: counter });
+function sendMatchData(data) {
+  chrome.runtime.sendMessage({ action: 'sendMatchData', matchData: data });
 }
